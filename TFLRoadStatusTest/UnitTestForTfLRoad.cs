39d3e4d10ca022c612f6d8e4c3a76906fed153ef
Libin Mathew <libin00@gmail.com>
2021-10-17 01:54:43 +0530

    Added new unit tests

--- TFLRoadStatusTest/UnitTestForTfLRoad.cs
+++ TFLRoadStatusTest/UnitTestForTfLRoad.cs
@@ -1,18 +1,67 @@
+using System;
+using System.IO;
 using NUnit.Framework;
+using TFLRoadStatus.CoreOperations.Models;
+using TFLRoadStatus.InputOutputOperations;
 
 namespace TFLRoadStatusTest
 {
     public class UnitTestForTfLRoad
     {
+        private UserInputOperations _inputOperations;
         [SetUp]
         public void Setup()
         {
+            _inputOperations = new UserInputOperations();
+        }
+
+        #region Tests for IO Operations
+
+        [Test]
+        public void PrintMessageToScreen()
+        {
+            var stringWriter = new StringWriter();
+            Console.SetOut(stringWriter);
+            var message = "Enter Road Code";
+            _inputOperations.PrintMessageToScreen(message);
+            // Console.WriteLine add new line character along with text
+            Assert.AreEqual(message + System.Environment.NewLine, stringWriter.ToString());
+        }
+        [Test]
+        public void ReadInputFromUser()
+        {
+            var stringReader = new StringReader("A2");
+            Console.SetIn(stringReader);
+            var message = _inputOperations.ReadInputFromUser();
+            Assert.AreEqual("A2", message);
         }
 
         [Test]
-        public void Test1()
-        {
-            Assert.Pass();
+        public void PrintInvalidRoadMessage()
+        { 
+            var stringWriter = new StringWriter();
+            Console.SetOut(stringWriter);
+            var message = "we is not a valid Road";
+            _inputOperations.PrintInvalidRoadMessage("we");
+            // Console.WriteLine add new line character along with text
+            Assert.AreEqual(message + System.Environment.NewLine, stringWriter.ToString());
         }
+
+        [Test]
+        public void PrintRoadStatusDetails()
+        {
+            RoadStatus roadStatus = new RoadStatus();
+            roadStatus.DisplayName = "A2";
+            roadStatus.StatusSeverity = "Good";
+            roadStatus.StatusSeverityDescription = "No Exceptional Delays";
+            var stringWriter = new StringWriter();
+            Console.SetOut(stringWriter);
+            var message = @"The status of the A2 is as follows\r\n Road Status is Good\r\n Road Status Description is No Exceptional Delays\r\n";
+            _inputOperations.PrintRoadStatusDetails(roadStatus);
+            // Console.WriteLine add new line character along with text
+            Assert.AreEqual(message, stringWriter.ToString());
+        }
+        #endregion
+
     }
 }
